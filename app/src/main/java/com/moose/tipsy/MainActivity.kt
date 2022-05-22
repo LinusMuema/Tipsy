0e4772cb73e3e24152e30c59675329f7e5ec6177
linusmuema <linus.m.muema@gmail.com>
2022-05-22 15:19:40 +0300

    - code cleanup and theme change

--- app/src/main/java/com/moose/tipsy/MainActivity.kt
+++ app/src/main/java/com/moose/tipsy/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.moose.tipsy
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.moose.tipsy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
